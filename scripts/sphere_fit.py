2f7907ce779995d7b09fe2a392664e923a63a0cf
Max Roen <maxwell-roen@users.noreply.github.com>
2022-04-01 10:51:37 -0400

    Fixes sphere_fit.py

--- scripts/sphere_fit.py
+++ scripts/sphere_fit.py
@@ -5,7 +5,6 @@ import numpy as np
 from geometry_msgs.msg import Point
 from robot_vision_lectures.msg import XYZarray
 from robot_vision_lectures.msg import SphereParams
-from sys import exit # for testing
 from math import sqrt
 
 
@@ -28,32 +27,21 @@ def fit_sphere():
 	global sphere_params_msg
 	B = []
 	A = []
-	# P = []
-	#print(type(pts))
-	#attrs = [name for name in dir(pts)]
-	#print(attrs)
-	num_pts = pts.__sizeof__()
 	just_pts = pts.points
+	num_pts = len(just_pts)
 	# extract the coordinates to form A and B
 	for i in range(num_pts):
 		x = just_pts[i].x
 		y = just_pts[i].y
 		z = just_pts[i].z
-		B.append([x ** 2, y ** 2, z ** 2])
+		B.append([x ** 2 + y ** 2 + z ** 2])
 		A.append([2 * x, 2 * y, 2 * z, 1])
 	# which we can use to solve for P
 	P = np.linalg.lstsq(A, B, rcond=None)[0]
-	#print(P) # I was expecting P to be a column vector but am getting a 4x3 matrix.
-	#xc = P[0]
-	#yc = P[1]
-	#zc = P[2]
-	#r = sqrt(P[3] + xc ** 2 + yc ** 2 + zc ** 2)
-	#print(f'xc: {xc}\nyc: {yc}\nzc: {zc}\nr: {r}')
-	# let's go ahead and return the test values from the slides so we can at least make sure the publisher is working
-	xc = 0.0
-	yc = 0.0
-	zc = 0.5
-	r = 0.04
+	xc = P[0]
+	yc = P[1]
+	zc = P[2]
+	r = sqrt(P[3] + xc ** 2 + yc ** 2 + zc ** 2)
 	# store the results in a SphereParams msg that we can publish
 	sphere_params_msg.xc = xc
 	sphere_params_msg.yc = yc
